bf211bebdc5e3b1d86ca3b81f4f61752b15ebea4
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-10-25 11:54:29 +0300

    Admin Files

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -26,8 +26,8 @@
 <!-- Sparkline -->
 <script src="{{ url('plugin_path') }}/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
 <!-- jvectormap -->
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="{{ url('plugin_path') }}/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="{{ url('plugin_path') }}/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 <!-- jQuery Knob Chart -->
 <script src="{{ url('plugin_path') }}/jquery-knob/dist/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
@@ -36,17 +36,17 @@
 <!-- datepicker -->
 <script src="{{ url('plugin_path') }}/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
 <!-- Bootstrap WYSIHTML5 -->
-<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script src="{{ url('plugin_path') }}/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
 <!-- Slimscroll -->
 <script src="{{ url('plugin_path') }}/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
 <script src="{{ url('plugin_path') }}/fastclick/lib/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="{{ url('js_path') }}/adminlte.min.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
+<script src="{{ url('js_path') }}/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<script src="{{ url('js_path') }}/demo.js"></script>
 </body>
 
 </html>
